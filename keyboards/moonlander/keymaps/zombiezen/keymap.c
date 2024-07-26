e718bd2a4820acb0677fda2a83880b367225a8d6
Roxy Light <roxy@zombiezen.com>
2024-07-25 17:46:31 -0700

    Move brackets to opposite sides of keyboard

--- keyboards/moonlander/keymaps/zombiezen/keymap.c
+++ keyboards/moonlander/keymaps/zombiezen/keymap.c
@@ -12,8 +12,8 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_6,                                           KC_TRANSPARENT, KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       KC_EQUAL,       
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_COLN,                                        KC_LBRC,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
-    LSFT(KC_LEFT_CTRL),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_SPACE,                                                                       KC_RBRC,        KC_H,           KC_J,           KC_K,           KC_L,           LT(3,KC_SCLN),  KC_QUOTE,       
+    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LBRC,                                        KC_RBRC,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
+    LSFT(KC_LEFT_CTRL),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_SPACE,                                                                       KC_TRANSPARENT, KC_H,           KC_J,           KC_K,           KC_L,           LT(3,KC_SCLN),  KC_QUOTE,       
     KC_LEFT_SHIFT,  KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RIGHT_SHIFT, 
     KC_GRAVE,       MO(1),          KC_LEFT_CTRL,   KC_LEFT_GUI,    KC_LEFT_ALT,    KC_ESCAPE,                                                                                                      KC_BSPC,        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       MO(1),          
     KC_TAB,         KC_LEFT_CTRL,   MO(2),                          KC_ENTER,       KC_RIGHT_ALT,   KC_SPACE
