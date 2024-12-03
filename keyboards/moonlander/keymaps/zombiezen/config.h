ab40f68b0d9da483061286c491496df68c3820a3
Roxy Light <roxy@zombiezen.com>
2024-12-03 15:58:57 -0800

    Revert breaking changes from configurator

--- keyboards/moonlander/keymaps/zombiezen/config.h
+++ keyboards/moonlander/keymaps/zombiezen/config.h
@@ -3,8 +3,11 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 
+#define ORYX_CONFIGURATOR
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define SERIAL_NUMBER "6p4NL/RpQox"
+#define FIRMWARE_VERSION u8"6p4NL/RpQox"
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE_ID 0x61
 #define LAYER_STATE_8BIT
 
 #define RGB_MATRIX_STARTUP_SPD 60
